6522b034fca009a08979f73ec84e74803578e618
Pratik Luitel <073bex426.pratik@pcampus.edu.np>
2020-07-03 10:40:20 +0545

    simpler gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,14 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     del = require('del'),
-    //browserSync = require('browser-sync'),
     imagemin = require('gulp-imagemin'),
     uglify = require('gulp-uglify'),
     usemin = require('gulp-usemin'),
     rev = require('gulp-rev'),
     cleanCss = require('gulp-clean-css'),
     flatmap = require('gulp-flatmap'),
-    htmlmin = require('gulp-htmlmin'),
-    //exec = require('gulp-exec'),
-    run =require('gulp-run-command').default;
+    run =require('gulp-run-command').default,
+    merge = require('merge-stream');
 
 //definition of the sass task, compiles the scss in /css/scss
 //and puts the compiles css in the same destination
@@ -33,27 +31,30 @@ gulp.task('watch', function () {
 
 gulp.task('jekyll', run('jekyll serve --source dist --dest dist/_site'));
 
-//browser sync task
-
-// Default task, is executed when we type 'gulp' to the terminal
-//gulp.task('default', gulp.parallel(gulp.series('watch')));
-
 //Cleans(deletes) the distribution(/dist/) directory, 
 gulp.task('clean', function(cb) {
    return del(['dist/**/*'],cb); // clean all except the git information
 });
 
 //copies fonts from the font-awesome dir
-gulp.task('copyfonts', function() {
- return gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
- .pipe(gulp.dest('./dist/fonts'));
-});
+gulp.task('copy', function() {
+  var fonts = gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+  .pipe(gulp.dest('./dist/fonts'));
+  var blog = gulp.src('./_posts/*')
+  .pipe(gulp.dest('./dist/_posts'));
+  var html = gulp.src('./*.html')
+  .pipe(gulp.dest('./dist/'));
+  var js = gulp.src('./dist/_layouts/js/*')
+  .pipe(gulp.dest('./dist/js/'));
+  var css = gulp.src('./dist/_layouts/css/*')
+  .pipe(gulp.dest('./dist/css/'));
+  var icon = gulp.src('*.png')
+  .pipe(gulp.dest('dist/'));
+  var cname = gulp.src('CNAME')
+  .pipe(gulp.dest('dist/'));
 
-gulp.task('copyblog', 
-  function(){
-   return gulp.src('./_posts/*')
-   .pipe(gulp.dest('./dist/_posts'));
-  });
+ return merge(fonts,blog,html,css,js,icon,cname);
+});
 
 // Image compression
 gulp.task('imagemin', function() {
@@ -78,32 +79,5 @@ gulp.task('usemin', function() {
    .pipe(gulp.dest('dist/_layouts/'))
 });
 
-//shell commands
-gulp.task('copyy', 
-  function(){
-   return gulp.src('./dist/_layouts/css/*')
-   .pipe(gulp.dest('./dist/css/'));
-  });
-
-gulp.task('copyyy', 
-  function(){
-   return gulp.src('./dist/_layouts/js/*')
-   .pipe(gulp.dest('./dist/js/'));
-  });
-
-  gulp.task('copytemp', function(){
-     return gulp.src('./*.html')
-     .pipe(gulp.dest('./dist/'));
-  })
-  gulp.task('copyicon', function(){
-    return gulp.src('*.png')
-    .pipe(gulp.dest('dist/'));
-  })
-  gulp.task('copyname', function(){
-    return gulp.src('CNAME')
-    .pipe(gulp.dest('dist/'));
-  })
-
-
-gulp.task('build',gulp.series('clean', gulp.parallel('copyfonts','copyblog','copytemp','imagemin','usemin'), 'copyy','copyyy','copyicon','copyname'));
+gulp.task('build',gulp.series('clean', 'usemin',gulp.parallel('copy','imagemin')));
 gulp.task('default',gulp.series('build', gulp.parallel('watch','jekyll')));
