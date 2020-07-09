2cf77e625da4f1dca375a149bb2befcfd8ab4a8c
Pratik Luitel <073bex426.pratik@pcampus.edu.np>
2020-07-09 20:27:36 +0545

    fully migrated to GatsbyJS, some cleanup left

--- src/pages/essays.js
+++ src/pages/essays.js
@@ -1,11 +1,56 @@
-import React, {Component} from "react"
+import React from "react"
 import Layout from '../Components/layout'
 import Navigation from "../Components/NavComponent"
-import PostsList from "../Components/PostsListComponent"
 import {Helmet} from 'react-helmet'
+import {Link, graphql, useStaticQuery} from 'gatsby'
 
-class Essays extends Component {
-  render(){
+function RenderCard({node}){
+  return(
+      <div className="card hoverable">
+          <div className="card-body">
+              <div className="imgcont">
+                  {node.frontmatter.image != null ? <img className="card-img-top" src={`${node.frontmatter.image}`} alt=""/>: null }
+                  <div id="date">
+                      {node.frontmatter.date}
+                  </div>
+              </div>
+              <div className="card-title">
+                  <p style={{fontSize: 'xx-large'}}>{node.frontmatter.title}</p>
+              </div>
+              <div className="card-content">
+                  <p>{node.excerpt}</p>
+              </div>  
+              <div className="card-content" style={{color:'grey', fontSize: 'small'}}>
+                  <p>{node.timeToRead} min read</p>
+              </div>
+              <Link to={node.fields.slug} className="stretched-link"> </Link>
+          </div>
+      </div>
+  )
+}
+
+export default function Essays() {
+
+  const data = useStaticQuery(graphql`
+  query essayQuery {
+    allMarkdownRemark(filter: {frontmatter: {category: {eq: "Essays"}}}) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          timeToRead
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            image
+            title
+          }
+        }
+      }
+    }
+  }
+`)
+  
     return (
       <Layout>
         <Helmet>
@@ -13,10 +58,33 @@ class Essays extends Component {
           <link rel="canonical" href="http://pratikl.com.np/essays" />
         </Helmet>
         <Navigation/>
-        <PostsList/>
+        <section className="Blog"  id="main">
+            <div className="text-center" id="bloghead">
+                <h1><strong>Posts</strong></h1>
+                <br />
+                <div className="col-10 offset-1 col-sm-6 offset-sm-3 col-md-4 offset-md-4" id="blognav">
+                    <div className="row align-self-center">
+                        <div className="col-4">
+                            <a href="/stories" className="stretched-link">Stories</a>
+                        </div>
+                        <div className="col-4 active">
+                            <a href="/essays" className="stretched-link">Essays</a>
+                        </div>
+                        <div className="col-4">
+                            <a href="/poems" className="stretched-link">Poems</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="col-sm-8 offset-sm-2">
+                <div className="row">
+                    {data.allMarkdownRemark.edges.map(
+                        ({node})=>(
+                            <div className="col-md-6" key={node.id}>{RenderCard({node})}</div>
+                        ))}
+                </div>
+            </div>
+        </section>
       </Layout>
     )
-  }
 }
-
-export default Essays;
--- src/pages/poems.js
+++ src/pages/poems.js
@@ -1,22 +1,90 @@
-import React, {Component} from "react"
+import React from "react"
 import Layout from '../Components/layout'
 import Navigation from "../Components/NavComponent"
-import PostsList from "../Components/PostsListComponent"
 import {Helmet} from 'react-helmet'
+import {Link, graphql, useStaticQuery} from 'gatsby'
 
-class Poems extends Component {
-  render(){
+function RenderCard({node}){
+  return(
+      <div className="card hoverable">
+          <div className="card-body">
+              <div className="imgcont">
+                  {node.frontmatter.image != null ? <img className="card-img-top" src={`${node.frontmatter.image}`} alt=""/>: null }
+                  <div id="date">
+                      {node.frontmatter.date}
+                  </div>
+              </div>
+              <div className="card-title">
+                  <p style={{fontSize: 'xx-large'}}>{node.frontmatter.title}</p>
+              </div>
+              <div className="card-content">
+                  <p>{node.excerpt}</p>
+              </div>  
+              <div className="card-content" style={{color:'grey', fontSize: 'small'}}>
+                  <p>{node.timeToRead} min read</p>
+              </div>
+              <Link to={node.fields.slug} className="stretched-link"> </Link>
+          </div>
+      </div>
+  )
+}
+
+export default function Poems() {
+
+  const data = useStaticQuery(graphql`
+  query poemQuery {
+    allMarkdownRemark(filter: {frontmatter: {category: {eq: "Poems"}}}) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          timeToRead
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            image
+            title
+          }
+        }
+      }
+    }
+  }
+`)
+  
     return (
       <Layout>
         <Helmet>
-          <title>Poems -- Pratik Luitel</title>
+          <title>Stories -- Pratik Luitel</title>
           <link rel="canonical" href="http://pratikl.com.np/poems" />
         </Helmet>
         <Navigation/>
-        <PostsList/>
+        <section className="Blog"  id="main">
+            <div className="text-center" id="bloghead">
+                <h1><strong>Posts</strong></h1>
+                <br />
+                <div className="col-10 offset-1 col-sm-6 offset-sm-3 col-md-4 offset-md-4" id="blognav">
+                    <div className="row align-self-center">
+                        <div className="col-4">
+                            <a href="/stories" className="stretched-link">Stories</a>
+                        </div>
+                        <div className="col-4">
+                            <a href="/essays" className="stretched-link">Essays</a>
+                        </div>
+                        <div className="col-4 active">
+                            <a href="/poems" className="stretched-link">Poems</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="col-sm-8 offset-sm-2">
+                <div className="row">
+                    {data.allMarkdownRemark.edges.map(
+                        ({node})=>(
+                            <div className="col-md-6" key={node.id}>{RenderCard({node})}</div>
+                        ))}
+                </div>
+            </div>
+        </section>
       </Layout>
     )
-  }
 }
-
-export default Poems;
