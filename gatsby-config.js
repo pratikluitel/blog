b5c5bc24bde6ba080e9980ca72c60eab6168cf66
Pratik Luitel <073bex426.pratik@pcampus.edu.np>
2020-07-10 10:44:47 +0545

    theme color changes for manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,8 +25,8 @@ module.exports = {
         name: "GatsbyJS",
         short_name: "GatsbyJS",
         start_url: "/",
-        background_color: "#6b37bf",
-        theme_color: "#6b37bf",
+        background_color: "#22a39f",
+        theme_color: "#22a39f",
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: "standalone",
